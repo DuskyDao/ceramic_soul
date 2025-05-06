403c2d8190a22fd17c3ac759df9fcee6f8323bad
DuskyDao <git.duskydog.mane@gmail.com>
2025-05-07 00:03:10 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,38 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
 
-// const vase = document.querySelector(".touch__decor")
-
-// vase.addEventListener("click", () => {
-//     console.log(vase)
-// });
+// init Swiper:
+try {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.icon-left-open',
+            prevEl: '.icon-right-open',
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 35,
+            },
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (error) { }
