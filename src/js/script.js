edb647a60a0d4c279f6bf79ac67e9dafbc714a1a
DuskyDao <git.duskydog.mane@gmail.com>
2025-05-07 22:51:25 +0300

    add about blog catalog

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,26 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
+// main css
 import "/src/sass/style.scss";
+
+
+// burger
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    // запрещаем скрол
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
 
 
 // init Swiper:
@@ -36,3 +55,26 @@ try {
         modules: [Navigation, Pagination],
     });
 } catch (error) { }
+
+// tabs
+try {
+    const tabs = document.querySelectorAll(".catalog__tab");
+    const contents = document.querySelectorAll(".catalog__content-item");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            // Удаляем активный класс у всех табов и контента
+            tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+            contents.forEach((c) => (c.style.display = "none"));
+
+            // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+            tab.classList.add("catalog__tab_active");
+            contents[index].style.display = "block";
+        });
+    });
+
+    // Показываем первый контент при загрузке
+    contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
+} catch (e) { }
+
+// Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
